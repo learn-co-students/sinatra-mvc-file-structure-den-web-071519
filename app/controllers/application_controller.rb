bf48ea94db656bdda7ac2df64aefd52b88ae9a70
SwanHub <jdprince555@gmail.com>
2019-08-06 15:45:50 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < Sinatra::Base
 
   configure do
-  	set :views, "app/views"
+  	set :views, "app/views" # tells the controller where to look to find views and the public directory. What is the public directory?
   	set :public_dir, "public"
   end
 
-  get "/" do
+  get "/" do # under the hood, Sinatra, via the "configure" method above, tells you to go to app/views/index (:index = index.erb).
   	erb :index
   end
 end
